f2b93840eb1897f1210995f3e863404957f38701
pantol <pantolik@rambler.ru>
2016-04-10 21:15:34 +0300

    Сделал проверку полей в цикле

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var myData=['#username','#password','#email','#gender','#credit_card','#bio'];
+var serverData=['Username','Password','Email','Gender','Credit Card','Bio'];
+
 (function($) {
     $(function() {
         $(document).on('click', '#send', function(event) {
-            var res;
+
             $('input.active').removeClass('active');
+            
             $.post('validator.php',
            {
                 'username': $('#username').val(),
@@ -13,60 +17,30 @@
                 'bio': $('#bio').val(),
             },
               function(data,myresult){
-                //res = $.parseJSON(data);
                 
-                    if(data.result){
-                        $('#response').html('Принято!');
-                        
-                    }
-                    else{
-                        $('#response').html('Неправильные данные:');
-                    }
+                if(data.result){
+                    $('#response').html('Принято!');
+                }
+                else{
+                    $('#response').html('Неправильные данные:');
+                }
                 
                 var myError = '<br>';
                 if (data.error){
-                    if (data.error.Username) {
-                        myError += data.error.Username+'<br>';
-                        $('#username').addClass('active');
+                    for(var i = 0 ; i < 6 ; i++ ){
+                        if (data.error[serverData[i]]) {
+                            myError += data.error[serverData[i]] +'<br>';
+                            $(myData[i]).addClass('active');
+                        }
                     }
-                    var my = '#password';
-                    if (data.error.Password) {
-                        myError += data.error.Password+'<br>';
-                        $(my).addClass('active');
-                    }
-                    if (data.error.Email) myError += data.error.Email+'<br>';
-                    if (data.error.Gender) myError += data.error.Gender+'<br>';
-                    var ccard = data.error['Credit Card'];
-                    if (ccard) myError += data.error['Credit Card']+'<br>';
-                    if (data.error.Bio) myError += data.error.Bio+'<br>';
                 }
                 
                 $('#response').html($('#response').html() +'<br>'+myError);
-                var mm = data.error;
-               
                 
-                console.log(data);
-                console.log(data.error['Username']);
-                console.log(data.error['Credit Card']);
-                //console.log(res[error]);
-                console.log(data.result);
-                //console.log(res.result.val());
-                
-                //console.log(mm.[0]);
-                //console.log(mm[2]);
-                
-                console.log(data);
-                console.log(data.error);
-                //console.log(data.error.index());
-                
-                console.log(data.result);
-                console.log(data);
-                
-                },"json");
-            });
-            event.preventDefault();
-
+            },"json");
+            
         });
-
+        event.preventDefault();
+    });
 })(jQuery);
 
